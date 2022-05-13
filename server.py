07f208cfd7c7138707d60585e08fe18dee6b3c7d
Shubham Srivastava <shbm@Shubhams-MacBook-Air.local>
2022-05-13 05:04:40 +0200

    heder fix

--- server.py
+++ server.py
@@ -11,6 +11,9 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.send_response(301)
             self.send_header('Location','https://drive.google.com/drive/folders/1EH-B_y-9JhCPLdEB7sAHNUU02QIkf-WB')
             self.end_headers()
+
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
         msg = 'Hello Shubham! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 

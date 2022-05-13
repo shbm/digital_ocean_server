d9f32c48b4268ac0e0f8bc06b66895c2018f57e3
Shubham Srivastava <shbm09@gmail.com>
2022-05-13 04:42:19 +0200

    redir

--- server.py
+++ server.py
@@ -7,8 +7,10 @@ from http import HTTPStatus
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
+        if self.path == "/drive":
+            self.send_response(301)
+            self.send_header('Location','https://drive.google.com/drive/folders/1EH-B_y-9JhCPLdEB7sAHNUU02QIkf-WB')
+            self.end_headers()
         msg = 'Hello Shubham! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
